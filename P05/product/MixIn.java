4e65f74f0a52e57955ee555683943decdc51037d
jgus02 <seg9917@mavs.uta.edu>
2022-10-02 10:21:18 -0500

    Created MixIn.java

---
+++ P05/product/MixIn.java
@@ -0,0 +1,19 @@
+package product;
+
+public class MixIn{
+    private MixInFlavor flavor;
+    private MixInAmount amount;
+
+    public MixIn(MixInFlavor flavor, MixInAmount amount){
+        this.flavor = flavor;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString(){
+        if(amount==MixInAmount.Normal){
+            return flavor;
+        }
+        return flavor + "(" + amount + ")";
+    }
+}
