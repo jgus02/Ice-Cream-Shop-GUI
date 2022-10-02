95a7661f0c34c9427d09ff9e57638c27e13aa567
jgus02 <seg9917@mavs.uta.edu>
2022-10-02 09:41:22 -0500

    Created P05 and added basic files.

---
+++ P05/product/Item.java
@@ -0,0 +1,13 @@
+public class Item{
+    String name;
+    String description;
+    int cost;
+    int price;
+
+    public Item(String name, String description, int cost, int price){
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+        this.price = price;
+    }
+}
