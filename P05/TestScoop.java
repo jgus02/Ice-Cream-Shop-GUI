49da59cd526690af0786e8c72077497283e25e47
jgus02 <seg9917@mavs.uta.edu>
2022-10-02 12:15:20 -0500

    Created basic outline.
    TODO: Add regression testing OR interactive testing

---
+++ P05/TestScoop.java
@@ -0,0 +1,14 @@
+import product.IceCreamFlavor;
+import product.Item;
+import product.MixIn;
+import product.MixInFlavor;
+import product.MixInAmount;
+import product.Scoop;
+import java.util.ArrayList;
+
+public class TestScoop{
+    ArrayList<IceCreamFlavor> flavors = new ArrayList<>();
+    public void main(String[] args){
+        
+    }
+}
