461e6204cf660f4af4591b88b74f3d54f83f009e
jgus02 <seg9917@mavs.uta.edu>
2022-10-02 10:05:33 -0500

    Created and completed MixInFlavor.java

---
+++ P05/product/MixInFlavor.java
@@ -0,0 +1,5 @@
+public class MixInFlavor extends Item(
+    public MixInFlavor(String name, String description, int cost, int price){
+        super(String name, String description, int cost, int price);
+    }
+) 
