c86d4f517916e5a8e1b9b7b95bfe4f3eab2bab75
jgus02 <seg9917@mavs.uta.edu>
2022-10-03 13:46:52 -0500

    SPRINT 1. Functional versions of IceCreamFlavor and Item
    TASK ID: 1
    FEATURE ID: CF

--- P05/product/IceCreamFlavor.java
+++ P05/product/IceCreamFlavor.java
@@ -1,4 +1,5 @@
 package product;
+
 public class IceCreamFlavor extends Item{
     public IceCreamFlavor(String name, String description, int cost, int price){
         super(name,description,cost,price);
--- P05/product/Item.java
+++ P05/product/Item.java
@@ -51,6 +51,6 @@ public abstract class Item{
         public IllegalIceCreamException(String message){
             super(message);
         }
-    };
+    }
 
 }
