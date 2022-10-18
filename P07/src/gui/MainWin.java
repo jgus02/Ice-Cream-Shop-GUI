da4ed8a4030295d6278656e2b0a84870573402c5
jgus02 <seg9917@mavs.uta.edu>
2022-10-18 02:13:45 -0500

    File I/O, updated about dialog, toolbar.
    Feature ID(s): LALL, SALL, SAVD, TOOLB, LOGO

--- P07/src/gui/MainWin.java
+++ P07/src/gui/MainWin.java
@@ -10,16 +10,15 @@ import javax.swing.JOptionPane;
 import javax.swing.JComponent;
 import javax.swing.JToolTip;
 import java.awt.event.ActionListener;
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.Box;
+
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
-/*import java.awt.Font;
-import java.awt.Color;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import java.awt.image.BufferedImage;*/
-
 import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -44,57 +43,58 @@ public class MainWin extends JFrame{
     public MainWin(String titleBar) throws IOException {
         super(titleBar); 
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //"It is recommended you only use this in
-        setSize(640, 360);                              //an application."  closes entire program? 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        setSize(640, 360);                             
         filename = new File("untitled.scp");
         emporium = new Emporium();
-        display  = new JLabel("");
+        display  = new JLabel("Welcome to MICE!");
         //
         // ---------- M E N U   B A R ----------
+        //
         JMenuBar menuBar = new JMenuBar();
 
-        JMenu     file           = new JMenu    ("File"); 
-        JMenuItem save           = new JMenuItem("Save");
-        JMenuItem saveAs         = new JMenuItem("Save As...");
-        JMenuItem open           = new JMenuItem("Open");
-        JMenuItem quit           = new JMenuItem("Quit");
-        save          .addActionListener(event -> onSaveClick());
-        saveAs        .addActionListener(event -> onSaveAsClick());
-        open          .addActionListener(event -> onOpenClick());
-        quit          .addActionListener(event -> onQuitClick());
-        file   .add(save);
-        file   .add(saveAs);
-        file   .add(open);
-        file   .add(quit);
+        JMenu     file          = new JMenu    ("File"); 
+        JMenuItem save          = new JMenuItem("Save");
+        JMenuItem saveAs        = new JMenuItem("Save As...");
+        JMenuItem load          = new JMenuItem("Load File...");
+        JMenuItem quit          = new JMenuItem("Quit");
+        save        .addActionListener(event -> onSaveClick());
+        saveAs      .addActionListener(event -> onSaveAsClick());
+        load        .addActionListener(event -> onLoadClick());
+        quit        .addActionListener(event -> onQuitClick());
+        file    .add(save);
+        file    .add(saveAs);
+        file    .add(load);
+        file    .add(quit);
 
-        JMenu     view           = new JMenu    ("View");
-        JMenuItem iceCreamView   = new JMenuItem("Ice Cream Flavors"); 
-        JMenuItem mixInView      = new JMenuItem("Mix-In Flavors");
-        JMenuItem scoopView      = new JMenuItem("Scoops");
-        iceCreamView  .addActionListener(event -> onIceCreamViewClick());
-        mixInView     .addActionListener(event -> onMixInViewClick());
-        scoopView     .addActionListener(event -> onScoopViewClick());
-        view   .add(iceCreamView);
-        view   .add(mixInView);
-        view   .add(scoopView);
+        JMenu     view          = new JMenu    ("View");
+        JMenuItem icfView       = new JMenuItem("Ice Cream Flavors"); 
+        JMenuItem mxfView       = new JMenuItem("Mix-In Flavors");
+        JMenuItem scpView       = new JMenuItem("Scoops");
+        icfView     .addActionListener(event -> onIcfViewClick());
+        mxfView     .addActionListener(event -> onMxfViewClick());
+        scpView     .addActionListener(event -> onScpViewClick());
+        view    .add(icfView);
+        view    .add(mxfView);
+        view    .add(scpView);
 
-        JMenu     create         = new JMenu    ("Create");
-        JMenuItem iceCreamCreate = new JMenuItem("Ice Cream Flavor");
-        JMenuItem mixInCreate    = new JMenuItem("Mix-In Flavor");
-        JMenuItem scoopCreate    = new JMenuItem("Scoop");
-        iceCreamCreate.addActionListener(event -> onIceCreamCreateClick());
-        mixInCreate   .addActionListener(event -> onMixInCreateClick());
-        scoopCreate   .addActionListener(event -> onScoopCreateClick());
-        create  .add(iceCreamCreate);
-        create  .add(mixInCreate);
-        create  .add(scoopCreate);
+        JMenu     create        = new JMenu    ("Create");
+        JMenuItem icfCreate     = new JMenuItem("Ice Cream Flavor");
+        JMenuItem mxfCreate     = new JMenuItem("Mix-In Flavor");
+        JMenuItem scpCreate   = new JMenuItem("Scoop");
+        icfCreate   .addActionListener(event -> onIcfCreateClick());
+        mxfCreate   .addActionListener(event -> onMxfCreateClick());
+        scpCreate   .addActionListener(event -> onScpCreateClick());
+        create  .add(icfCreate);
+        create  .add(mxfCreate);
+        create  .add(scpCreate);
 
-        scoopCreate.setToolTipText("Create a flavor first!");
-        scoopCreate.setEnabled(false);  //must have at least one ice cream flavor to use
+        scpCreate.setToolTipText("Create a flavor first!");
+        scpCreate.setEnabled(false);  //must have at least one ice cream flavor to use
 
-        JMenu     help           = new JMenu    ("Help");
-        JMenuItem about          = new JMenuItem("About");
-        about         .addActionListener(event -> onAboutClick());
+        JMenu     help          = new JMenu    ("Help");
+        JMenuItem about         = new JMenuItem("About");
+        about       .addActionListener(event -> onAboutClick());
         help    .add(about);
 
         menuBar.add(file);
@@ -103,16 +103,81 @@ public class MainWin extends JFrame{
         menuBar.add(help);
         setJMenuBar(menuBar);
         //
-        // ---------- T O O L   B A R ----------        //TODO
-        /*JToolBar toolbar = new JToolBar("Options");
+        //---------- T O O L   B A R ----------
+        //        
+        JToolBar toolbar = new JToolBar("Options");
 
-        JButton iceCreamCreateB = new JButton*/ 
+        JButton saveB       
+            = new JButton(new ImageIcon(this.getClass().getResource("resources/save.png")));
+        saveB      .setActionCommand("Save changes");
+        saveB      .setToolTipText("Save changes");
+        JButton saveAsB     
+            = new JButton(new ImageIcon(this.getClass().getResource("resources/saveAs.png")));
+        saveAsB    .setActionCommand("Save file as...");
+        saveAsB    .setToolTipText("Save file as...");
+        JButton loadB 
+            = new JButton(new ImageIcon(this.getClass().getResource("resources/load.png")));
+        loadB      .setActionCommand("Load file...");
+        loadB      .setToolTipText("Load file...");
+        saveB      .addActionListener(event -> onSaveClick());
+        saveAsB    .addActionListener(event -> onSaveAsClick());
+        loadB      .addActionListener(event -> onLoadClick()); 
+        toolbar.add(saveB);
+        toolbar.add(saveAsB);
+        toolbar.add(loadB);
+            
+        toolbar.add(Box.createHorizontalStrut(20));   
+
+        JButton icfViewB  
+            = new JButton(new ImageIcon(this.getClass().getResource("resources/icfView.png")));
+        icfViewB   .setActionCommand("View ice cream flavors");
+        icfViewB   .setToolTipText("View ice cream flavors");
+        JButton mxfViewB  
+            = new JButton(new ImageIcon(this.getClass().getResource("resources/mxfView.png")));
+        mxfViewB   .setActionCommand("View mix-in flavors");
+        mxfViewB   .setToolTipText("View mix-in flavors");
+        JButton scpViewB  
+            = new JButton(new ImageIcon(this.getClass().getResource("resources/scpView.png")));
+        scpViewB   .setActionCommand("View scoops");
+        scpViewB   .setToolTipText("View scoops");
+        icfViewB   .addActionListener(event -> onIcfViewClick());
+        mxfViewB   .addActionListener(event -> onMxfViewClick());
+        scpViewB  .addActionListener(event -> onScpViewClick());
+        toolbar.add(icfViewB);
+        toolbar.add(mxfViewB);
+        toolbar.add(scpViewB);
+        
+        toolbar.add(Box.createHorizontalStrut(10));
+
+        JButton icfCreateB  
+            = new JButton(new ImageIcon(getClass().getResource("resources/icfCreate.png")));
+        icfCreateB .setActionCommand("Create new ice cream flavor");
+        icfCreateB .setToolTipText("Create new ice cream flavor");
+        JButton mxfCreateB  
+            = new JButton(new ImageIcon(getClass().getResource("resources/mxfCreate.png")));
+        mxfCreateB .setActionCommand("Create new mix-in flavor");
+        mxfCreateB .setToolTipText("Create new mix-in flavor");
+        JButton scpCreateB  
+            = new JButton(new ImageIcon(getClass().getResource("resources/scpCreate.png")));
+        scpCreateB .setActionCommand("Create new scoop");
+        scpCreateB .setToolTipText("Create new scoop");
+        icfCreateB .addActionListener(event -> onIcfCreateClick());
+        mxfCreateB .addActionListener(event -> onMxfCreateClick());
+        scpCreateB .addActionListener(event -> onScpCreateClick());
+        toolbar.add(icfCreateB);
+        toolbar.add(mxfCreateB);
+        toolbar.add(scpCreateB);
+        
+        getContentPane().add(toolbar, BorderLayout.PAGE_START);
+
         setVisible(true);
         
         this.add(display);
     }
 
-    // -------- File  Listeners ---------
+    //
+    // ------------ L I S T E N E R S ------------
+    // -------- FILE LISTENERS ---------
     protected void onSaveClick() {
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(filename))){
             bw.write(MAGIC_COOKIE + "\n");
@@ -142,7 +207,7 @@ public class MainWin extends JFrame{
         }
     }
 
-    protected void onOpenClick(){
+    protected void onLoadClick(){
         final JFileChooser fc = initializeFileChooser();
         if(fc.showOpenDialog(this) == JFileChooser.APPROVE_OPTION){
             filename = fc.getSelectedFile();
@@ -154,7 +219,7 @@ public class MainWin extends JFrame{
                 throw new RuntimeException("Incompatible MICE file format.");
             }
                emporium = new Emporium(br);
-            if (emporium.iceCreamFlavors().length != 0){
+            if (emporium.icf().length != 0){
                 this.getJMenuBar().getMenu(2).getItem(2).setEnabled(true);
             }
 
@@ -178,56 +243,77 @@ public class MainWin extends JFrame{
         System.exit(0);
     }
 
-    //
-    // --------- View Listeners ----------
-    protected void onIceCreamViewClick(){
+    
+    // ---------VIEW LISTENSTERS----------
+    protected void onIcfViewClick(){
         view(Screen.ICE_CREAM_FLAVORS);
     }
 
-    protected void onMixInViewClick(){
+    protected void onMxfViewClick(){
         view(Screen.MIX_IN_FLAVORS);
     }
 
-    protected void onScoopViewClick(){
+    protected void onScpViewClick(){
         view(Screen.SCOOPS);
     }
-    //
-    // ------- Create Listeners ------------
-    protected void onIceCreamCreateClick(){ //TODO: INEFFICIENT SCOOPCREATE DISABLING METHOD?
+    
+    // -------CREATE LISTENERS------------
+    protected void onIcfCreateClick(){
         CreateIceCreamDialog flavorDialog = new CreateIceCreamDialog(this);
         if(flavorDialog.success){
-            JMenuItem tempScoopCreate = this.getJMenuBar().getMenu(2).getItem(2);
-            if(!tempScoopCreate.isEnabled()){
-                tempScoopCreate.setToolTipText(null);
-                tempScoopCreate.setEnabled(true);
+            JMenuItem tempScpCreate = this.getJMenuBar().getMenu(2).getItem(2);
+            if(!tempScpCreate.isEnabled()){
+                tempScpCreate.setToolTipText(null);
+                tempScpCreate.setEnabled(true);
             }
-            emporium.addIceCreamFlavor(flavorDialog.getChoice());
+            emporium.addIcf(flavorDialog.getChoice());
+            updateDisplayOnCreation(Screen.ICE_CREAM_FLAVORS);
         }
     }
 
-    protected void onMixInCreateClick(){
+    protected void onMxfCreateClick(){
         CreateMixInDialog flavorDialog = new CreateMixInDialog(this);
         if(flavorDialog.success){
-            emporium.addMixInFlavor(flavorDialog.getChoice());
+            emporium.addMxf(flavorDialog.getChoice());
+            updateDisplayOnCreation(Screen.MIX_IN_FLAVORS);
         }
     }
 
-    protected void onScoopCreateClick(){
+    protected void onScpCreateClick(){
         CreateScoopDialog scoopDialog = new CreateScoopDialog(this, this.emporium);
         if(scoopDialog.success){
-            emporium.addScoop(scoopDialog.getChoice());
+            emporium.addScp(scoopDialog.getChoice());
+            updateDisplayOnCreation(Screen.SCOOPS);
         }
     }
-    //
-    // --------- Help Listeners -------------
-    protected void onAboutClick(){
-        JOptionPane.showMessageDialog(null, 
-                        "Ice Cream Emporium\n\nCopyright 2022 Jasper Gustafson" + 
-                        " - Licensed under Gnu GPL 3.0\n\n" + 
-                        "Create ice cream and topping flavors and order them as servings of ice cream"
-        );
+    
+    private void updateDisplayOnCreation(Screen screen){ //this seems over-engineered
+        char tmp;                                      //also if I change view's text 
+        switch(screen){                                //I'll have to change this
+            case ICE_CREAM_FLAVORS:
+                tmp = 'I';
+                break;
+            case MIX_IN_FLAVORS:
+                tmp = 'M';
+                break;
+            case SCOOPS:
+                tmp = 'S';
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid screen.");
+        }
+            if(display.getText().charAt(9) == tmp){
+                view(screen);
+            }
     }
 
+    // ------------HELP LISTENERS-------------
+    protected void onAboutClick(){
+        AboutDialog about = new AboutDialog(this);
+    }
+    //
+    //  ----------  M A I N    P A N E L  -----------
+    //
     private void view(Screen screen){
         Object[] arr = {};
         int i = 1;
@@ -236,18 +322,18 @@ public class MainWin extends JFrame{
         switch(screen){
             case ICE_CREAM_FLAVORS:
                 displayBuilder.append("ICE CREAM FLAVORS");
-                arr = emporium.iceCreamFlavors();
+                arr = emporium.icf();
                 break;
             case MIX_IN_FLAVORS:
                 displayBuilder.append("MIX-IN FLAVORS");
-                arr = emporium.mixInFlavors();
+                arr = emporium.mxf();
                 break;
             case SCOOPS:
                 displayBuilder.append("SCOOPS");
-                arr = emporium.scoops();
+                arr = emporium.scp();
                 break;
             default:
-                throw new IllegalArgumentException("View must contain value from screen enum.");
+                throw new IllegalArgumentException("Invalid screen.");
         }
 
         displayBuilder.append("</b><br><br>");
@@ -271,11 +357,13 @@ public class MainWin extends JFrame{
         display.setText(displayBuilder.toString());
     }
 
+    // ------------------------------
+
     private String FILE_VER = "1.0";
     private String MAGIC_COOKIE = "SCOOP🍦";
     private File filename;
 
-    private String VERSION = "1.3";
+    protected String VERSION = "1.3";
 
     private Emporium emporium;
     private JLabel display;
