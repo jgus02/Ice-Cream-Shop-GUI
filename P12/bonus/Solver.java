95b9d8f7da893c324a3a68a42da7a12c7e8d8ae2
jgus02 <seg9917@mavs.uta.edu>
2022-11-30 23:21:39 -0600

    Bonus

---
+++ P12/bonus/Solver.java
@@ -0,0 +1,84 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Solver {
+    public static void main(String[] args) {
+        double  min = -1000;           // Start searching for roots at x = min
+        double  max = 1000;            // Stop  searching for roots at x = max
+        double  slices = 100000000;    // Check this many points between min and max
+        int     threads = 1;           // Use this many threads to search
+        double  precision = 0.000001;  // When a root is detected, narrow to this precision
+        boolean log = false;           // Whether to print log messages to STDERR or not
+        boolean fail = false;          // Set true if any initialization fails (e.g., bad filename)
+        
+        if(args.length == 0) {
+            System.err.println("usage: java Solver [--precision p] [--threads t] [--slices s] [--min n] [--max m] [--log] file.poly...");
+            System.err.println("  precision - solve until within this much of the actual root (default " + precision + ")");
+            System.err.println("  threads   - use this many threads to solve each polynomial  (default " + threads + ")");
+            System.err.println("  slices    - divide min-max range into this many subranges   (default " + slices + ")");
+            System.err.println("  min       - start searching for roots at this x             (default " + min + ")");
+            System.err.println("  max       - stop  searching for roots at this x             (default " + max + ")");
+            System.err.println("  log       - print log messages to STDERR                    (default " + (log ? "print" : "no" + " messages)"));
+            System.err.println("  file.poly - one or more polynomial files to load and solve");
+            System.exit(0);
+        }
+        
+        Map <String, Polynomial> polynomials = new HashMap<>();
+        
+        int index = -1;
+        while(++index < args.length) {
+            if(args[index].equals(   "--precision")) precision = Double.parseDouble(args[++index]);
+            else if(args[index].equals("--threads")) threads   = Integer.parseInt  (args[++index]);
+            else if(args[index].equals( "--slices")) slices    = Double.parseDouble(args[++index]);
+            else if(args[index].equals(    "--min")) min       = Double.parseDouble(args[++index]);
+            else if(args[index].equals(    "--max")) max       = Double.parseDouble(args[++index]);
+            else {
+                System.out.print("Loading " + args[index] + "... ");
+                try (BufferedReader br = new BufferedReader(new FileReader(args[index]))) {
+                    polynomials.put(args[index], new Polynomial(br));
+                    System.out.println("done");
+                } catch(IOException e) {
+                    System.out.println("FAIL\n" + e);
+                    fail = true;
+                }
+            }
+        }
+
+        Polynomial.log = log; // Set true to print log messages to STDERR, false by default
+                
+        if(fail) System.exit(-1);
+        System.out.println("\nthreads = " + threads + ": range " + min + " to " + max + ", " + slices + " slices, precision = " + precision + "\n");
+        ArrayList<Thread> activeThreads = new ArrayList<>();
+        int i = 0;
+        final double finalMin = min;
+        final double finalMax = max;
+        final int finalThreads = threads;  
+        final double finalSlices = slices;
+        final double finalPrecision = precision;
+        for(var file : polynomials.keySet()) {
+            Polynomial p = polynomials.get(file);
+            Runnable runnable = new Runnable() {
+                public void run() {
+                    p.solve(finalMin, finalMax, finalThreads, finalSlices, finalPrecision);
+                    System.out.println(file + ": " + p.toString() + " has " + p.roots().length + " roots\n  " 
+                             + Arrays.toString(p.roots()) + "\n");
+                }
+            };
+            activeThreads.add(new Thread(runnable));
+            activeThreads.get(activeThreads.size()-1).start();
+        }
+        for(Thread thread : activeThreads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("Error: Thread was interrupted.");
+            }
+        }
+    }
+}
